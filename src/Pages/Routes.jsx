3659f6685fbc6055e79df850baa7f1de47178510
Vsept17 <verzchseptian@gmail.com>
2022-09-10 09:03:05 +0700

    config css & routing

--- src/Pages/Routes.jsx
+++ src/Pages/Routes.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Home from "./Home";
 
 const Routes = () => {
   return (
     <BrowserRouter>
       <Switch>
-        <Route />
+        <Route exact component={Home} path="/" />
       </Switch>
     </BrowserRouter>
   );
